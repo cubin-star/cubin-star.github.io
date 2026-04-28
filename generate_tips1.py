bcd66f9ae62db539ab2ad305c042f9719aa4f850
cubin-star <cubin@post.cz>
2026-04-28 09:39:11 +0200

    Update regex in is_second_tier for league detection
    Enhanced the regex in is_second_tier function to include additional league terms.

--- generate_tips1.py
+++ generate_tips1.py
@@ -181,7 +181,7 @@ def is_blocked_team(team_name):
 def is_second_tier(name):
     """Returns True if the league is NOT the top first division (i.e. 2nd tier and below)."""
     return bool(re.search(
-        r"\b(2|3|4|5|II|III|IV|V|segunda|tercera|championship|league two|league one|serie b|serie c|ligue 2|2\. liga|3\. liga|2\. bundesliga|3\. bundesliga|eerste divisie|second|third|play-?off|playoffs?|cup|pokal|coupe|copa|taca)\b",
+        r"\b(2|3|4|5|II|III|IV|V|segunda|tercera|championship|league two|league one|serie b|serie c|ligue 2|2\. liga|3\. liga|2\. bundesliga|3\. bundesliga|eerste divisie|second|third|play-?off|playoffs?|cup|pokal|coupe|copa|taca|non.?league|non league|northern|southern|isthmian|nacional b|promotion)\b",
         name, re.IGNORECASE
     ))
 
